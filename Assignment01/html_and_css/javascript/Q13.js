85c2f54a430dc28b62f2430e41d9568563f45a9a
Rupesh Singh <rupeshsingh938997@gmail.com>
2024-12-15 10:40:49 +0530

    Your commit message here

---
+++ Assignment01/html_and_css/javascript/Q13.js
@@ -0,0 +1,15 @@
+const myPromise=(boolenvalue)=>{
+    return new Promise((resolve,reject)=>{
+        if(boolenvalue){
+            resolve("Promise resolved")
+        } else{
+            reject("Promise rejected")
+        }
+    })
+}
+
+myPromise(true).then((res)=>{
+    console.log(res)
+}).catch((err)=>{
+    console.log(err)
+})
